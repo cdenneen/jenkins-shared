2fc15412146f78fade1af5d77486581a7a0d083f
Chris Denneen <cdenneen@gmail.com>
2017-05-05 15:12:44 -0400

    Update puppetTesting.groovy

--- vars/puppetTesting.groovy
+++ vars/puppetTesting.groovy
@@ -1,4 +1,4 @@
-def call() {
+def call(Closure body) {
     podTemplate(label: 'puppet', containers:[
         containerTemplate(name: 'ruby21', image: 'ruby:2.1-alpine', ttyEnabled: true, command: 'cat'),
         containerTemplate(name: 'ruby22', image: 'ruby:2.2-alpine', ttyEnabled: true, command: 'cat'),
