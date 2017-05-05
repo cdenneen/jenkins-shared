1068597f773db0bb51c4ad02aa568466d700cc54
Chris Denneen <cdenneen@gmail.com>
2017-05-05 15:02:47 -0400

    Create puppetTesting.groovy

---
+++ vars/puppetTesting.groovy
@@ -0,0 +1,19 @@
+def call() {
+    podTemplate(label: 'puppet', containers:[
+        containerTemplate(name: 'ruby21', image: 'ruby:2.1-alpine', ttyEnabled: true, command: 'cat'),
+        containerTemplate(name: 'ruby22', image: 'ruby:2.2-alpine', ttyEnabled: true, command: 'cat'),
+        containerTemplate(name: 'ruby23', image: 'ruby:2.3-alpine', ttyEnabled: true, command: 'cat')
+    ]){
+        node('puppet') {
+                container('ruby21'){
+                    body()
+                }
+                container('ruby22'){
+                    body()
+                }
+                container('ruby23'){
+                    body()
+                }
+        }
+    }
+}
