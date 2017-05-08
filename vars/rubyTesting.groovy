7df2561d770e80e9be0d4b74d4fb0668d02ca410
Chris Denneen <cdenneen@gmail.com>
2017-05-08 11:32:51 -0400

    Updated rubyTesting

--- vars/rubyTesting.groovy
+++ vars/rubyTesting.groovy
@@ -1,22 +1,24 @@
 #!/usr/bin/groovy
 
 def call(String[] rubyVersions = ['ruby21', 'ruby22', 'ruby23'], Closure body) {
-    podTemplate(label: 'rubypod', containers:[
-        containerTemplate(name: 'ruby21', image: 'ruby:2.1', ttyEnabled: true, command: 'cat'),
-        containerTemplate(name: 'ruby22', image: 'ruby:2.2', ttyEnabled: true, command: 'cat'),
-        containerTemplate(name: 'ruby23', image: 'ruby:2.3', ttyEnabled: true, command: 'cat')
-    ]){
-        node('rubypod') {
-            def rubyMatrix = [:]
-            for (int i = 0; i < rubyVersions.size(); i++) {
-                def index = i
-                rubyMatrix["${rubyVersions[index]}"] = {
-                    container(name: "${rubyVersions[index]}"){
-                        body()
-                    }
+  podTemplate(label: 'rubypod', containers:[
+    containerTemplate(name: 'ruby21', image: 'ruby:2.1', ttyEnabled: true, command: 'cat'),
+    containerTemplate(name: 'ruby22', image: 'ruby:2.2', ttyEnabled: true, command: 'cat'),
+    containerTemplate(name: 'ruby23', image: 'ruby:2.3', ttyEnabled: true, command: 'cat')
+  ]){
+    node('rubypod') {
+      def rubyMatrix = [:]
+      for (int i = 0; i < rubyVersions.size(); i++) {
+          def index = i
+          rubyMatrix["${rubyVersions[index]}"] = {
+              container(name: "${rubyVersions[index]}"){
+                withEnv("RUBY_VERSION=${rubyVersions[index]}"){
+                  body()
                 }
-            }
-            parallel rubyMatrix
-        }
+              }
+          }
+      }
+      parallel rubyMatrix
     }
+  }
 }
