365a2f52c1996ff17be43bb19b66a84b1af6a536
Chris Denneen <cdenneen@gmail.com>
2017-05-08 11:51:53 -0400

    Updates for env

--- vars/puppetTesting.groovy
+++ vars/puppetTesting.groovy
@@ -5,10 +5,14 @@ def call(String[] puppetVersions = ['4.7.1', '4.8.1', '4.9.4'], Closure body) {
   for (int i = 0; i < puppetVersions.size(); i++) {
     def index = i
     puppetMatrix["${puppetVersions[index]}"] = {
-      withEnv("PUPPET_GEM_VERSION=${puppetVersions[index]}") {
-        puppetPrep
-        puppetLint
-        puppetSpec
+      puppetPrep {
+        withEnv("PUPPET_GEM_VERSION=${puppetVersions[index]}")
+      }
+      puppetLint {
+        withEnv("PUPPET_GEM_VERSION=${puppetVersions[index]}")
+      }
+      puppetSpec {
+        withEnv("PUPPET_GEM_VERSION=${puppetVersions[index]}")
       }
     }
   }
--- vars/rubyTesting.groovy
+++ vars/rubyTesting.groovy
@@ -12,7 +12,7 @@ def call(String[] rubyVersions = ['ruby21', 'ruby22', 'ruby23'], Closure body) {
         def index = i
         rubyMatrix["${rubyVersions[index]}"] = {
           container(name: "${rubyVersions[index]}"){
-            body(withEnv("RUBY_VERSION=${rubyVersions[index]}"))
+            body()
           }
         }
       }
