8668c8077a7eed4b8589ec06caf62b9130349986
Chris Denneen <cdenneen@gmail.com>
2017-05-08 10:19:18 -0400

    Updated rubyTesting

--- vars/rubyTesting.groovy
+++ vars/rubyTesting.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/groovy
 
-def call(Array rubyVersions = ['ruby21', 'ruby22', 'ruby23'], Closure body) {
+def call(String[] rubyVersions = ['ruby21', 'ruby22', 'ruby23'], Closure body) {
     podTemplate(label: 'rubypod', containers:[
         containerTemplate(name: 'ruby21', image: 'ruby:2.1', ttyEnabled: true, command: 'cat'),
         containerTemplate(name: 'ruby22', image: 'ruby:2.2', ttyEnabled: true, command: 'cat'),
