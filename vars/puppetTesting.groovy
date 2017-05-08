a70392543deab3ec4866cbb72cd4ff6550563a4e
Chris Denneen <cdenneen@gmail.com>
2017-05-08 10:52:59 -0400

    Updated puppetTesting

--- vars/puppetTesting.groovy
+++ vars/puppetTesting.groovy
@@ -5,8 +5,8 @@ def call(String[] puppetVersions = ['4.7.1', '4.8.1', '4.9.4'], Closure body) {
   for (int i = 0; i < puppetVersions.size(); i++) {
     def index = i
     puppetMatrix["${puppetVersions[index]}"] = {
-      withEnv("PUPPET_GEM_VERSION=${puppetVersions[index]}") {
-        rubyTesting{
+      rubyTesting{
+        withEnv("PUPPET_GEM_VERSION=${puppetVersions[index]}") {
           body()
         }
       }
