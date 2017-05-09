aa6989421233f0a0a8755d4ed2c31d96249f3f1a
Chris Denneen <cdenneen@gmail.com>
2017-05-09 18:03:20 -0400

    need to {} the functions in order for them to run

--- vars/puppetTesting.groovy
+++ vars/puppetTesting.groovy
@@ -7,9 +7,9 @@ def call(String[] puppetVersions = ['4.7.1', '4.8.1', '4.9.4'], Closure body) {
 //    puppetMatrix["${puppetVersions[index]}"] = {
       stage("${puppetVersions[index]}"){
         withEnv(["PUPPET_GEM_VERSION=${puppetVersions[index]}","STRICT_VARIABLES=no"]){
-          puppetPrep
-          puppetLint
-          puppetUnit
+          puppetPrep {}
+          puppetLint {}
+          puppetUnit {}
         }
       }
 //    }
